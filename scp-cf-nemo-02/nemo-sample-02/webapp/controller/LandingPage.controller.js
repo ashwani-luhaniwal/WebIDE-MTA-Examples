ced954dcf952304bbc764e882c4f96c0c4de646c
ashwani-luhaniwal <ashwani.kumar.luhaniwal@sap.com>
2017-11-21 00:36:13 +0530

    LandingPage Controller code

--- scp-cf-nemo-02/nemo-sample-02/webapp/controller/LandingPage.controller.js
+++ scp-cf-nemo-02/nemo-sample-02/webapp/controller/LandingPage.controller.js
@@ -46,6 +46,158 @@ sap.ui.controller("scp.cf.nemo-sample-01.controller.LandingPage", {
         sap.ui.getCore().byId('slbl_desig').setVisible(true);
         sap.ui.getCore().byId('slbl_gender').setVisible(true);
 
-        
+        sap.ui.getCore().byId('s_txtName').setText(empdata[0].name);
+        sap.ui.getCore().byId('s_txtDob').setText(empdata[0].dob);
+        sap.ui.getCore().byId('s_txtdesig').setText(empdata[0].designation);
+        sap.ui.getCore().byId('s_txtgender').setText(empdata[0].gender);
+    },
+
+    handleOperationBtncreate: function(oEvent) {
+        var empData = {
+            "id": sap.ui.getCore().byId('ip_cEmpId').getValue(),
+            "name": sap.ui.getCore().byId('ip_cName').getValue(),
+            "dob": sap.ui.getCore().byId('ip_cDOB').getValue(),
+            "gender": sap.ui.getCore().byId('ip_cGender').getValue(),
+            "designation": sap.ui.getCore().byId('ip_cDesig').getValue()
+        };
+
+        var aData = jQuery.ajax({
+            type: "POST",
+            url: "/employee",
+            dataType: "json",
+            data: empData,
+            async: false,
+            success: function(response, status) {
+                console.log(response + status);
+            }
+        });
+
+        var oModel = new sap.ui.model.json.JSONModel();
+        var aData = jQuery.ajax({
+            type: "GET",
+            contentType: "application/json",
+            url: "/employees",
+            dataType: "json",
+            async: false,
+            success: function(data, textStatus, jqXHR) {
+                oModel.setData({modelData: data});
+            }
+        });
+        sap.ui.getCore().setModel(oModel, 'model_table');
+        var oTable = sap.ui.getCore().byId("tbl_odata");
+        oTable.setModel(oModel);
+        oTable.bindRows("/modelData");
+
+        landingpagecontroller.CRUDselection.close();
+
+        sap.ui.getCore().byId('ip_cEmpId').setValue(''),
+        sap.ui.getCore().byId('ip_cName').setValue(''),
+        sap.ui.getCore().byId('ip_cDOB').setValue(''),
+        sap.ui.getCore().byId('ip_cGender').setValue(''),
+        sap.ui.getCore().byId('ip_cDesig').setValue('')
+    },
+
+    handlebtn_Delete: function(oEvent) {
+        jQuery.ajax({
+            url: "/employee/" + sap.ui.getCore().byId('ip_EmpId').getValue(),
+            type: "DELETE",
+            dataType: "json",
+            success: function(response, status) {
+
+            }
+        });
+
+        var oModel = new sap.ui.model.json.JSONModel();
+        var aData = jQuery.ajax({
+            type: "GET",
+            contentType: "application/json",
+            url: "/employees",
+            dataType: "json",
+            async: false,
+            success: function(data, textStatus, jqXHR) {
+                oModel.setData({modelData: data});
+            }
+        });
+        sap.ui.getCore().setModel(oModel, 'model_table');
+        var oTable = sap.ui.getCore().byId("tbl_odata");
+        oTable.setModel(oModel);
+        oTable.bindRows("/modelData");
+        landingpagecontroller.CRUDselection.close();
+
+        landingpagecontroller.CRUDselection.close();
+    },
+
+    handlebtn_Save: function(oEvent) {
+        var empData = {
+            "id": sap.ui.getCore().byId('ip_EmpId').getValue(),
+            "name": sap.ui.getCore().byId('ip_Name').getValue(),
+            "dob": sap.ui.getCore().byId('ip_DOB').getValue(),
+            "gender": sap.ui.getCore().byId('ip_Gender').getValue(),
+            "designation": sap.ui.getCore().byId('ip_Desig').getValue()
+        };
+        jQuery.ajax({
+            url: "/employee/" + sap.ui.getCore().byId('ip_EmpId').getValue(),
+            type: "PUT",
+            dataType: "json",
+            data: empData,
+            success: function(response, status) {
+
+            }
+        });
+        var oModel = new sap.ui.model.json.JSONModel();
+        var aData = jQuery.ajax({
+            type: "GET",
+            contentType: "application/json",
+            url: "/employees",
+            dataType: "json",
+            async: false,
+            success: function(data, textStatus, jqXHR) {
+                oModel.setData({modelData: data});
+            }
+        });
+        sap.ui.getCore().setModel(oModel, 'model_table');
+        var oTable = sap.ui.getCore().byId("tbl_odata");
+        oTable.setModel(oModel);
+        oTable.bindRows("/modelData");
+        landingpagecontroller.CRUDselection.close();
+    },
+
+    handleOperationBtncls: function(oEvent) {
+        landingpagecontroller.CRUDselection.close();
+    },
+
+    rowSelect: function(e) {
+        var idx = e.getParameter('rowIndex');
+        var name = (sap.ui.getCore().getModel('model_table').getProperty('/modelData/' + idx).name);
+        var EmpId = (sap.ui.getCore().getModel('model_table').getProperty('/modelData/' + idx).id);
+        var Desig = (sap.ui.getCore().getModel('model_table').getProperty('/modelData/' + idx).designation);
+        var DOB = (sap.ui.getCore().getModel('model_table').getProperty('/modelData/' + idx).dob);
+        var gender = (sap.ui.getCore().getModel('model_table').getProperty('/modelData/' + idx).gender);
+        sap.ui.getCore().byId('ip_EmpId').setValue(EmpId);
+        sap.ui.getCore().byId('ip_Name').setValue(name);
+        sap.ui.getCore().byId('ip_DOB').setValue(DOB);
+        sap.ui.getCore().byId('ip_Desig').setValue(Desig);
+        sap.ui.getCore().byId('ip_Gender').setValue(gender);
+        landingpagecontroller.CRUDselection.open();
+        sap.ui.getCore().byId('ip_EmpId').setEnabled(false);
+    },
+
+    getData: function() {
+        var oModel = new sap.ui.model.json.JSONModel();
+        var aData = jQuery.ajax({
+            type: "GET",
+            contentType: "application/json",
+            url: "/employees",
+            dataType: "json",
+            async: false,
+            success: function(data, textStatus, jqXHR) {
+                oModel.setData({modelData: data});
+            }
+        });
+
+        sap.ui.getCore().setModel(oModel, 'model_table');
+        var oTable = sap.ui.getCore().byId("tbl_odata");
+        oTable.setModel(oModel);
+        oTable.bindRows("/modelData");
     }
-})
+});

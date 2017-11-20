b90db1d14eae38991bd348fe35ba69f023aabdff
ashwani-luhaniwal <ashwani.kumar.luhaniwal@sap.com>
2017-11-20 13:42:40 +0530

    Adding git ignore in nemo-sample-01

--- scp-cf-nemo-01/nemo-sample-01/app.js
+++ scp-cf-nemo-01/nemo-sample-01/app.js
@@ -1,13 +1,14 @@
+"use strict";
 
-var express = require("express");
-var bodyParser = require("body-parser");
-var cfenv = require("cfenv");
+let express = require("express");
+let bodyParser = require("body-parser");
+let cfenv = require("cfenv");
 
 // create express instance
-var oApp = express();
+let oApp = express();
 
 // Cloud Foundry environment variables
-var oAppEnv = cfenv.getAppEnv();
+let oAppEnv = cfenv.getAppEnv();
 
 // body parser middleware to handle URL parameter and JSON bodies
 oApp.use(bodyParser.urlencoded({ extended: false }));
@@ -27,5 +28,5 @@ require("./server/api/info/info.js")(oApp, oAppEnv);
 require("./server/api/users/users.js")(oApp);
 
 oApp.listen(oAppEnv.port, function() {
-	console.log("Server listening at " + oAppEnv.port);
+	console.log("Server listening at " + oAppEnv.url);
 });
